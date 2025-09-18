18ed2ab19010ee66969faad2056a60ecc841e439
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:26:44 -0300

    first commit

---
+++ Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace EnumComposition2.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace EnumComposition2.Entities.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
